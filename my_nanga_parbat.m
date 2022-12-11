1757ba139bd0916a046bde1cf20259471aa91329
Siddhant Gajbhiye <120318794+SiddhantGajbhiye05@users.noreply.github.com>
2022-12-12 01:03:27 +0530

    Update my_nanga_parbat.m

--- my_nanga_parbat.m
+++ my_nanga_parbat.m
@@ -1,3 +1,14 @@
+% The Nanga Parbat in the western Himalayas is a growing mountain. Each year, its elevation increases by
+% 7 mm. If its present height is 8, 126 meters, plot its changing elevation for the next 20 years as a function of
+% time. If somehow, the rate of its growth was to increase by 20% after the first 10 years, then how would its
+% height grow as a function of time? Make a plot to show this on the same axis as the constant-rate growth time
+% series so that one can compare the two time-series easily. Make sure that the two time-series have legends
+% and different colors so that one can easily identify which time series he/she is looking at. Remember, an axis
+% without labels conveys no information! Write all of this in a neat script my_nanga_parbat.m
+
+
+
+
 % step 1: plotting for growth rate 7mm
 t = 1:20;
 ho = 8126;
